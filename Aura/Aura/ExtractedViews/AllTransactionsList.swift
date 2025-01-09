35199701cbced20d66a91e7076b956eaa2ec8d4b
Julien Cotte <julien.cotte@live.fr>
2025-01-09 14:26:02 +0100

    add accountDetailView

---
+++ Aura/Aura/ExtractedViews/AllTransactionsList.swift
@@ -0,0 +1,18 @@
+//
+//  AllTransactionsList.swift
+//  Aura
+//
+//  Created by Julien Cotte on 09/01/2025.
+//
+
+import SwiftUI
+
+struct AllTransactionsList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllTransactionsList()
+}
---
+++ Aura/Aura/Views/AccountDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountDetailView.swift
+//  Aura
+//
+//  Created by Julien Cotte on 09/01/2025.
+//
+
+import SwiftUI
+
+struct AccountDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountDetailView()
+}
